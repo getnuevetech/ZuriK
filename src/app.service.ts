cf6677deb8c0c6219993937be786aefcc5c53cfd
AG <ag@richardwinfred.com>
2026-02-19 08:47:22 -0500

    Update app.service.ts with the AppService class implementation

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,17 +1,8 @@
-import { Injectable, OnApplicationBootstrap } from '@nestjs/common';
-import { DatabaseService } from './database.service';
+import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AppService implements OnApplicationBootstrap {
-  constructor(private readonly databaseService: DatabaseService) {}
-
-  async onApplicationBootstrap() {
-    console.log('Application is bootstrapping, starting database seed.');
-    try {
-      await this.databaseService.seedDatabase();
-      console.log('Database seeding completed successfully.');
-    } catch (error) {
-      console.error('Error during database seeding:', error);
-    }
+export class AppService {
+  getHello(): string {
+    return 'African Fashion API';
   }
 }
